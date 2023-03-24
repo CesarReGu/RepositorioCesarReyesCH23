1c92839c14eb8bb73327f4a87f80275e9d9603a4
Cesar-Regu <cesaromar.reyesgut@gmail.com>
2023-03-24 14:33:35 -0700

    Actualizacion

---
+++ JavaWeb/jw-01sts/anotaciones/src/main/java/com/generation/cohorte23/app/model/Libro.java
@@ -0,0 +1,40 @@
+package com.generation.cohorte23.app.model;
+
+public class Libro {
+	private String titulo, autor, isbn;
+
+	public Libro() {
+
+	}
+
+	public Libro(String titulo, String autor, String isbn) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.isbn = isbn;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+}
